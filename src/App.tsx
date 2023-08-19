a185d4fbf0ed129a80c9a4db39ca7a9c0837ee0d
Erfan <erfan80amani23@email.com>
2023-08-19 18:46:08 +0330

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="p-6 h-[80%] w-[500px] bg-indigo-100 rounded-xl">
+        <div className="flex flex-col gap-4 items-center justify-center"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
